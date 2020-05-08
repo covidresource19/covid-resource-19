ee9a1cf55a6110a81a20b570b97b978f808154e1
ManavJain2210 <manavjain709@gmail.com>
2020-05-08 12:59:51 +0530

    Merge

---
+++ src/screens/FindNearestHospital.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, ImageBackground, TouchableOpacity, TextInput, StyleSheet , ScrollView} from 'react-native';
+import firestore from '@react-native-firebase/firestore'
+
+export default class FindNearestHospital extends React.Component{
+    render(){
+        return(
+            <View>
+                <Text>Hospital</Text>
+            </View>
+        )
+    }
+}
